d9b6e20954a4c5506d9ecfab54b3d43b2139d1b3
기남후 <skagn4929@naver.com>
2024-01-11 21:44:43 +0900

    Params & Queries

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,18 +1,4 @@
-const fetchPost = async (postId: string) => {
-  const post = await fetch(`/api/posts/${postId}`, {
-    method: "GET",
-  });
-  return post.json();
-};
-
-export async function generateMetadata({ params }: any) {
-  const { post } = await fetchPost(params.id);
-  return {
-    title: post[0].title,
-    description: post[0].description,
-  };
-}
-
-export default function PostID({ params }: any) {
-  return <div>Post {params.id}</div>;
+export default function PostID({ params, searchParams }: any) {
+  console.log(searchParams);
+  return <main>Post {searchParams.new}</main>;
 }
