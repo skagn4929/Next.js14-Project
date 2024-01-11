6087cd4205e078470e4bcd0a777daa1b2704a839
기남후 <skagn4929@naver.com>
2024-01-11 19:30:51 +0900

    components part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+
+import Header from "@/components/Header";
+import Logo from "@/components/Header/Logo";
+
 export default function Home() {
-  return <div>main page of my app</div>;
+  return (
+    <div>
+      <Header name={"Namhoo"} age={30} isMan={true}>
+        <Logo />
+      </Header>
+    </div>
+  );
 }
