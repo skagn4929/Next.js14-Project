f6d58a142ef3e83f47da44d187b54c603c31e6e8
기남후 <skagn4929@naver.com>
2024-01-11 18:52:30 +0900

    Link part

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function Profile() {
@@ -9,11 +10,9 @@ export default function Profile() {
     <div>
       <h1>my list of profiles</h1>
       <div>
-        <ul>
-          <li onClick={() => router.push("/profile/1")}>Profile 1</li>
-          <li onClick={() => router.push("/profile/2")}>Profile 2</li>
-          <li onClick={() => router.push("/profile/3")}>Profile 3</li>
-        </ul>
+        <Link href="/profile/1">Profile 1</Link>
+        <Link href="/profile/2">Profile 2</Link>
+        <Link href="/profile/3">Profile 3</Link>
       </div>
     </div>
   );
